26d6916cd767d39df192b23dbb0e2e0c758fe799
MajesticDarkTiger <darktiger533@gmail.com>
2023-04-11 15:41:20 -0500

    Add basic JS examples

--- app/index.js
+++ app/index.js
@@ -1,1 +1,53 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+const catalog = [
+  {
+    id: "1",
+    item: "Canned Beans",
+    price: 3.99,
+    category: "Food",
+    quantity: 25,
+  },
+  {
+    id: "2",
+    item: "Shirts",
+    price: 8.99,
+    category: "Clothing",
+    quantity: 50,
+  },
+  {
+    id: "3",
+    item: "Laptop",
+    price: 599.99,
+    category: "Electronics",
+    quantity: 5,
+  },
+  {
+    id: "4",
+    item: "Samsung Galaxy s5",
+    price: 699.99,
+    category: "Electronics",
+    quantity: 6,
+  },
+  {
+    id: "5",
+    item: "Pants",
+    price: 10.99,
+    category: "Clothing",
+    quantity: 50,
+  },
+];
+
+const user = {
+  username: "larythetiger",
+  cart: [],
+};
+
+// console.log(catalog, user);
+// console.log(catalog[2].price.toFixed(2));
+// toFixed(2) to get how many decimals you put in the ()
+
+// function add2Numbers(num1, num2) {
+//   return num1 + num2;
+// }
+
+// const total = add2Numbers(2, 4);
+// console.log(total);
